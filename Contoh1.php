154b00ee3a6f166d00c285ee9bbdb4370a13c0d9
Rizallmaulana <rizalmln2004@gmail.com>
2024-03-24 00:56:24 +0700

    Tugas Pertemuan 1

---
+++ Contoh1.php
@@ -0,0 +1,12 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo"Nama saya Rizal Maulana
+            Saya tinggal di daerah Cakung
+            olah raga yang saya sukai adalah
+            Bulutangkis";
+    }
+}
